ace59b25f73a2a87797d48db5d11fd08366cce03
Carlos <mezerotm@gmail.com>
2016-11-19 08:47:06 -0500

    Changed initial Parent to LoginScreen.fxml

--- Provider/src/practitioner_desktop/Main.java
+++ Provider/src/practitioner_desktop/Main.java
@@ -8,14 +8,23 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static Stage primaryStage;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("initialLayout.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        this.primaryStage = primaryStage;
+        //title of application
+        primaryStage.setTitle("Practitioner Desktop Application");
+
+        //layouts
+        Parent initial = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+        //scenes
+        Scene practitionerContent = new Scene(initial);
+
+        //set initial scene & display
+        primaryStage.setScene(practitionerContent);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
