8f4f1fb38f6ddb6e63e5470f873a6b92b83354c9
Carlos <mezerotm@gmail.com>
2016-11-19 02:30:44 -0500

    Added MainContentListView, Added Sample Data

--- Provider/src/practitioner_desktop/Controller.java
+++ Provider/src/practitioner_desktop/Controller.java
@@ -1,4 +1,57 @@
 package practitioner_desktop;
 
-public class Controller {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ListView<String> mainContentListView;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mainContentListView.setItems(FXCollections.observableArrayList(
+                "Patient #1 : [Subject] - Severity",
+                "Patient #2 : [Subject] - Severity",
+                "Patient #3 : [Subject] - Severity",
+                "Patient #4 : [Subject] - Severity",
+                "Patient #5 : [Subject] - Severity",
+                "Patient #6 : [Subject] - Severity",
+                "Patient #7 : [Subject] - Severity",
+                "Patient #8 : [Subject] - Severity",
+                "Patient #9 : [Subject] - Severity",
+                "Patient #10 : [Subject] - Severity",
+                "Patient #11 : [Subject] - Severity",
+                "Patient #12 : [Subject] - Severity",
+                "Patient #13 : [Subject] - Severity",
+                "Patient #14 : [Subject] - Severity",
+                "Patient #15 : [Subject] - Severity",
+                "Patient #16 : [Subject] - Severity",
+                "Patient #17 : [Subject] - Severity",
+                "Patient #17 : [Subject] - Severity",
+                "Patient #18 : [Subject] - Severity",
+                "Patient #19 : [Subject] - Severity",
+                "Patient #20 : [Subject] - Severity",
+                "Patient #21 : [Subject] - Severity",
+                "Patient #22 : [Subject] - Severity",
+                "Patient #23 : [Subject] - Severity"
+        ));
+
+
+        mainContentListView.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+
+            }
+        });
+    }
 }
